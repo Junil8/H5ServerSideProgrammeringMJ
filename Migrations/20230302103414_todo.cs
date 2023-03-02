a702e5b5e775628e58c03ddfd6dba6e265aaa1fc
Julian <just-julian@hotmail.com>
2023-03-02 11:35:03 +0100

    Migration, Scaffolding, JCodes

---
+++ Migrations/20230302103414_todo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace H5ServerSideProgrammeringMJ.Migrations
+{
+    /// <inheritdoc />
+    public partial class todo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TaskDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,12 +15,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var identityConnectionString = builder.Configuration.GetConnectionString("IdentityConnection") ?? throw new InvalidOperationException("Connection string 'IdentityConnection' not found.");
-//var todoConnectionString = builder.Configuration.GetConnectionString("ToDoConnection") ?? throw new InvalidOperationException("Connection string 'ToDoConnection' not found.");
+var todoConnectionString = builder.Configuration.GetConnectionString("ToDoConnection") ?? throw new InvalidOperationException("Connection string 'ToDoConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(identityConnectionString));
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(todoConnectionString));
+
+builder.Services.AddDbContext<ToDoContext>(options =>
+    options.UseSqlServer(todoConnectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -29,13 +30,10 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 
-// added from tut
-builder.Services.AddDbContext<ToDoContext>();
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddControllersWithViews();
-
-//var context = services.GetRequiredService<ToDoContext>();
-//ToDoInitializer.Initialize(context);
+//// added from tut
+//builder.Services.AddDbContext<ToDoContext>();
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
