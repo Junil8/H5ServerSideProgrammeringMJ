f8388ff015660d4958a61b0d799d2206923ff91f
Julian <just-julian@hotmail.com>
2023-03-03 05:54:12 +0100

    Added comments

--- JCodes/ToDoDBHandler.cs
+++ JCodes/ToDoDBHandler.cs
@@ -6,6 +6,7 @@ namespace H5ServerSideProgrammeringMJ.JCodes
 {
     public class ToDoDBHandler
     {
+        //-----------------------------------------------------------
         // Create
         public ToDo Create(string taskName, string TaskDescription, string username, ToDoContext context)
         {
@@ -19,18 +20,22 @@ namespace H5ServerSideProgrammeringMJ.JCodes
             context.SaveChanges();
             return todo;
         }
-        // Read
+        //-----------------------------------------------------------
+        // ReadAll
         public List<ToDo> ReadAll(string username, ToDoContext context)
         {
             List<ToDo> todoList = context.ToDos.Where(x => x.User == username).ToList();
 
             return todoList;
         }
+        //-----------------------------------------------------------
+        // ReadById
         public ToDo ReadById(string username, int id, ToDoContext context)
         {
             ToDo todo = context.ToDos.Where(a => a.User == username).FirstOrDefault(x => x.Id == id);
             return todo;
         }
+        //-----------------------------------------------------------
         // Update
         public ToDo Update(string taskName, string TaskDescription, int id, ToDoContext context)
         {
@@ -42,6 +47,7 @@ namespace H5ServerSideProgrammeringMJ.JCodes
             context.SaveChanges();
             return result;
         }
+        //-----------------------------------------------------------
         // Delete
         public ToDo Delete(int id, ToDoContext context)
         {
